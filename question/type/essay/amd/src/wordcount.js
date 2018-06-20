d21d3cd59ea34c89f1d33357b5c2c20a9ac69433
Simeon Naydenov <moniNaydenov@gmail.com>
2018-06-20 09:41:56 +0200

    remove error in ReadOnly mode

--- question/type/essay/amd/src/wordcount.js
+++ question/type/essay/amd/src/wordcount.js
@@ -108,12 +108,14 @@ define([
                    this.ctx[$params.editorname] = $params;
                    this.lastTimeout = null;
                    var self = this;
-                   M.mod_quiz.wordcount.update = function() {
-                       self.update_wordcount();
-                   };
-                   M.mod_quiz.autosave.form.on('change', this.update_wordcount, this);
-                   this.init_tinymce();
-                   this.update_wordcount();
+                   if (M.mod_quiz) {
+                       M.mod_quiz.wordcount.update = function() {
+                           self.update_wordcount();
+                       };
+                       M.mod_quiz.autosave.form.on('change', this.update_wordcount, this);
+                       this.init_tinymce();
+                       this.update_wordcount();
+                   }
                }
            };
        });
